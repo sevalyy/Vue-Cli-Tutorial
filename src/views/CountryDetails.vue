886c3c3c49a09d7e723aa9087da6aad652d7105a
sevalyy <seval@yamanyar.com>
2022-11-14 15:41:14 +0100

    get single country for details page

--- src/views/CountryDetails.vue
+++ src/views/CountryDetails.vue
@@ -1,13 +1,44 @@
 <template>
-  <h1>Country Details</h1>
-  <p>
-    {{ id }}
-  </p>
+  <div v-if="country">
+    <h1>{{ country.name.official }}</h1>
+    <h2>{{ country.name.nativeName }}</h2>
+    <h3>Region</h3>
+    {{ country.region }}
+    <h3>Capital</h3>
+    {{ country.capital[0] }}
+    <h3>Languages</h3>
+
+    {{ country.languages }}
+
+    <h3>Currency</h3>
+    {{ country.currencies }}
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   props: ["id"],
+
+  mounted() {
+    this.getSingleCountry();
+  },
+  data() {
+    return {
+      country: null,
+      errors: [],
+    };
+  },
+  methods: {
+    getSingleCountry() {
+      axios
+        .get(`https://restcountries.com/v3.1/alpha?codes=${this.id}`)
+        .then((response) => (this.country = response.data[0]))
+        .catch((error) => {
+          this.errors.push(error);
+        });
+    },
+  },
 };
 </script>
 
